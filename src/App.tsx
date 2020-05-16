dcca107c631bd9de647416792deaa8c4ff61b4e9
Albert-dias <albert.uft@gmail.com>
2020-05-15 22:25:10 -0300

    initialization api context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
